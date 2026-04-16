4e9cdb235b8065106d3ca9eff2025dd4edd0eedc
AhamedMusthafa20304 <ahamedmusthafa20304@gmail.com>
2026-04-16 19:04:09 +0530

    fix wildcard route final

--- server.ts
+++ server.ts
@@ -119,9 +119,10 @@ async function startServer() {
   // Vite middleware for development
  
     app.use(express.static(path.join(__dirname, 'dist')));
-    app.get('/*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-    });
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
   app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
